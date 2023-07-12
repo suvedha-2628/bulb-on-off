125c7d750e61ef2fff6947cc47cc5d10ae6a2d30
suvedha-2628 <139156605+suvedha-2628@users.noreply.github.com>
2023-07-12 11:15:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var on_btn=document.getElementById("on");
+var off_btn=document.getElementById("off");
+var img=document.getElementById("bulb");
+function bulb_on(){
+    img.src="./image/bulb_on.jpg";
+}
+function bulb_off(){
+    img.src="./image/bulb_off.jpg";
+}
